223c57ba8803d6b2c686ca37e1d7421f9bb3c59e
Rakshanda Kahilkar <rakshanda.k@icedq.ai>
2026-02-26 18:28:16 +0530

    fixed content not showing issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,3 @@
-export const dynamic = "force-dynamic";
-
-async function getHomePage() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_WP_API}/pages?slug=home`,
-    { cache: "no-store" }
-  );
-
-  if (!res.ok) return null;
-
-  const data = await res.json();
-  return data.length ? data[0] : null;
-}
-
-export default async function Home() {
-
-  const page = await getHomePage();
-
-  if (!page) {
-    return <div>Page not found</div>;
-  }
-
-  return (
-    <main
-      dangerouslySetInnerHTML={{
-        __html: page?.content_full || ""
-      }}
-    />
-  );
+export default function Home() {
+  return <h1>Next.js App Router Working 🚀</h1>
 }
